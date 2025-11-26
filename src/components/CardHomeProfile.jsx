47a57fc4473bce207852e9e3a698bdd14ee79c08
JuanDiegoCC02 <dcorella2324@gmail.com>
2025-11-26 10:40:26 -0600

    Updating NavPortfolio, FooterPortfolio and CardHomeProfile Styles

--- src/components/CardHomeProfile.jsx
+++ src/components/CardHomeProfile.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import "../styles/CardHomeProfile.css";
 import ft_Profile from "../images/Ft Profile.webp"
+import photoProfile from "../images/photoProfile.png"
+
 import { Link } from 'react-router-dom';
 
 function CardHomeProfile() {
   return (
-    <div>
+    <div className='AllCardHomeProfile'>
 
       <div className='CardFtProfileHome'>
         <Link to="/profile">
-        <img src={ft_Profile} alt="Ft Profile" width={150} height={150} className='FtProfileHome' />
+        <img src={photoProfile} alt="Ft Profile" width={150} height={150} className='FtProfileHome' />
         </Link>
       </div>
 
