c034d3045304ae766dfda58c93873de8fbe21923
Juan Diego C.C. <dcorella2324@gmail.com>
2026-08-07 22:59:20 -0600

    feat: create upcomingProjects.js new structure in src/data.

---
+++ src/data/upcomingProjects.js
@@ -0,0 +1,41 @@
+import newProjects from "../images/newProjects.png";
+
+const upcomingProjects = {
+    id: "upcoming-projects",
+
+    title: "Upcoming Projects",
+
+    description:
+        "New projects are currently in development. This section will be updated as new applications and technical projects are completed.",
+
+    github: "https://github.com/JuanDiegoCC02",
+
+    technologies: {
+        languages: [
+            "In Progress"
+        ],
+
+        frontend: [
+            "In Progress"
+        ],
+
+        backend: [
+            "In Progress"
+        ],
+
+        tools: [
+            "In Progress"
+        ]
+    },
+
+    gallery: [
+        {
+            src: newProjects,
+            alt: "Upcoming projects",
+            description:
+                "New projects and technical developments will be added to the portfolio as they are completed."
+        }
+    ]
+};
+
+export default upcomingProjects;
